371a17798f1917db036a2bd222608741d02c607a
hsythesun <87266294+hsythesun@users.noreply.github.com>
2025-01-11 14:49:37 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,14 +8,6 @@ export default defineConfig({
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, './src'),
-		},
-	},
-	build: {
-		rollupOptions: {
-			output: {
-				manualChunks: undefined,
-			},
-		},
-		assetsDir: 'assets',
+		},``
 	},
 })
