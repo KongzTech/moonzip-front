f30f2d8d34e5c32c3671baa76e84668708621ba7
hsythesun <87266294+hsythesun@users.noreply.github.com>
2025-01-13 00:37:04 +0300

    updated table

--- src/components/TableView.tsx
+++ src/components/TableView.tsx
@@ -1,4 +1,5 @@
 import BadgeIcon from '@/assets/icons/BadgeIcon'
+import ChevronRightIcon from '@/assets/icons/ChevronRightIcon'
 import { Coin } from '@/types/coin'
 import { formatNumber } from '@/utils/formatNumber'
 
@@ -31,7 +32,7 @@ const formatGraduationTime = (graduationDate: Date | undefined): string => {
 export default function TableView({ coins }: { coins: Coin[] }) {
 	return (
 		<div className='w-full overflow-x-auto'>
-			<table className='w-full bg-dark-700 rounded-[10px] overflow-hidden border border-dark-700 border-collapse'>
+			<table className='w-full lg:bg-dark-700 rounded-[10px] overflow-hidden lg:border lg:border-dark-700 border-collapse'>
 				<thead className='bg-dark-800 hidden lg:table-header-group w-full rounded-t-[10px]'>
 					<tr className='text-left text-sm font-ibm-mono uppercase text-light-100'>
 						<th className='px-4 py-3'>Token</th>
@@ -45,9 +46,9 @@ export default function TableView({ coins }: { coins: Coin[] }) {
 					{coins.map(coin => (
 						<tr
 							key={coin.ca}
-							className='border-t border-dark-700 bg-dark-800 text-light-100 font-ibm-mono font-semibold uppercase text-sm'
+							className='border-t border-dark-700 bg-dark-800 text-light-100 font-ibm-mono font-semibold uppercase text-sm lg:table-row flex items-center'
 						>
-							<td className='px-4 py-4 lg:py-3 '>
+							<td className='pr-2 lg:px-4 py-5 flex-1 lg:py-3 '>
 								<div className='flex items-center  gap-2.5 lg:gap-4'>
 									<div
 										className={` w-[62px] hidden lg:flex items-center gap-1 ${
@@ -80,7 +81,7 @@ export default function TableView({ coins }: { coins: Coin[] }) {
 									</div>
 								</div>
 							</td>
-							<td className='px-3 py-3'>
+							<td className='pr-2 py-3 lg:px-4 w-[100px] lg:w-auto'>
 								<div className='flex flex-col text-xs lg:text-sm lg:flex-row gap-1'>
 									<div>
 										{isMobile
@@ -107,22 +108,31 @@ export default function TableView({ coins }: { coins: Coin[] }) {
 									{coin.lock ? coin.lock.toString() + 'D ' : 'NO '}
 								</div>
 							</td>
-							<td className='p-4 flex flex-col text-xs lg:text-sm gap-1'>
-								<div
-									className={` w-[62px] flex lg:hidden items-center ${
-										coin.score === 100 ? 'icn-purple' : ''
-									}`}
-								>
-									<BadgeIcon className='scale-75' />
-									<span
-										className={`${
-											coin.score === 100 ? 'text-purple-100' : 'text-light-100'
-										} `}
+							<td className='lg:px-4 py-3 flex lg:table-cell items-center text-xs lg:text-sm justify-between'>
+								<div className='flex flex-col lg:items-center lg:flex-row gap-1'>
+									<div
+										className={`h-[16px] w-[62px] flex lg:hidden items-center ${
+											coin.score === 100 ? 'icn-purple' : ''
+										}`}
 									>
-										{coin.score ? (coin.score / 10).toFixed(1) : '0.0'}
-									</span>
+										<BadgeIcon className='scale-75 ' />
+										<span
+											className={`${
+												coin.score === 100
+													? 'text-purple-100'
+													: 'text-light-100'
+											} `}
+										>
+											{coin.score ? (coin.score / 10).toFixed(1) : '0.0'}
+										</span>
+									</div>
+									<div className='flex-1'>
+										{formatGraduationTime(coin.graduation)}
+									</div>
+									<ChevronRightIcon className='hidden lg:block' />
 								</div>
-								<div>{formatGraduationTime(coin.graduation)}</div>
+
+								<ChevronRightIcon className='lg:hidden' />
 							</td>
 						</tr>
 					))}
